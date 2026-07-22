56cc5407f835acb06baf0411365412baf04174a8
wretchedworm <wretchedworm@gmail.com>
2026-07-22 17:10:52 +0800

    Sync Password Wording Update 2
    Deconflicts the problem of old working fighting itself making people wonder why uniqueness matters at all.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fajt-hours-v8";
-const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=13", "./calculations.js?v=13", "./sync.js?v=13", "./config.js?v=13", "./manifest.webmanifest"];
+const CACHE = "fajt-hours-v9";
+const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=14", "./calculations.js?v=14", "./sync.js?v=14", "./config.js?v=14", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
