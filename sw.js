87bd43afe18738de07228bac43bf11ac8ca8e7a6
wretchedworm <wretchedworm@gmail.com>
2026-07-22 17:44:10 +0800

    App labels change for consistency

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fajt-hours-v12";
-const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=17", "./calculations.js?v=17", "./sync.js?v=17", "./config.js?v=17", "./manifest.webmanifest"];
+const CACHE = "fajt-hours-v13";
+const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=18", "./calculations.js?v=18", "./sync.js?v=18", "./config.js?v=18", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
