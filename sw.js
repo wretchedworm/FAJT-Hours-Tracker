5797cebf7ae489712d28f01e02f8043596de3934
wretchedworm <wretchedworm@gmail.com>
2026-07-22 17:24:08 +0800

    Sync Password wording update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fajt-hours-v9";
-const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=14", "./calculations.js?v=14", "./sync.js?v=14", "./config.js?v=14", "./manifest.webmanifest"];
+const CACHE = "fajt-hours-v11";
+const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=16", "./calculations.js?v=16", "./sync.js?v=16", "./config.js?v=16", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
